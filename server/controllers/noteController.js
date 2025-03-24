928de98bb369880350439bbce270b73d9fdd2e1d
whyh <a1729328500@163.com>
2025-03-24 16:31:26 +0800

    项目初始化

---
+++ server/controllers/noteController.js
@@ -0,0 +1,22 @@
+import poll from "../config/db";
+
+//创建笔记
+export const createNote = async (req, res) => {
+  try {
+    const { userId, title, content, categoryId, tags } = req.body;
+    const [result] = await poll.query(
+      "INSERT INTO notes (userId,title,content,categoryId,tags) VALUES (?,?,?,?,?)",
+      [userId, title, content, categoryId, JSON.stringify(tags)]
+    );
+    res.status(201).json({
+      id: result.insertId,
+      userId,
+      title,
+      content,
+      categoryId,
+      tags,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
